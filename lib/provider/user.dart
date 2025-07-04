56fc8cea8ccbe5249136e3dfe08a70cf26ee015f
Muhammad Ali <kustalics@gmail.com>
2025-07-04 15:04:40 +0500

    Provider Done

--- lib/provider/user.dart
+++ lib/provider/user.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:ibad_project/models/notification.dart';
 
 class UserProvider extends ChangeNotifier {
   String _name = '';
+  NotificationModel? _model;
 
   void setName(String val) {
     _name = val;
     notifyListeners();
   }
 
+  void setNotification(NotificationModel model) {
+    _model = model;
+    notifyListeners();
+  }
+
   String getName() => _name;
+
+  NotificationModel? getNotification() => _model;
 }
